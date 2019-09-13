3707bfc3afa1bc7ac658918c740661954b091394
unknown <schreckengostb1@gmail.com>
2019-09-12 23:00:57 -0700

    initial script push

---
+++ script.js
@@ -0,0 +1,68 @@
+var dropdown = "<li><a href=\"#\" onclick=\"return dropdownClicked(this, event)\" class=\"has-submenu\"><span class=\"sub-arrow\">+</span><span id=\"filter-ability-text\">All Abilities</span></a><ul id=\"filter-ability-selection-container\" style=\"display: none;\"><table class=\"letter-and-ability-table\"><tbody><tr></tr></tbody></table><table class=\"actor-menu-table bordered\"><tbody><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-64\" style=\"\" onclick=\"return setAbilityFilter(303657, 0, this)\" oncontextmenu=\"setAbilityFilter(303657, 0, this)\">Arcane Burst</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=303657\">ID: 303657</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-64\" style=\"\" onclick=\"return setAbilityFilter(298781, 0, this)\" oncontextmenu=\"setAbilityFilter(298781, 0, this)\">Arcane Orb</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=298781\">ID: 298781</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-64\" style=\"\" onclick=\"return setAbilityFilter(302999, 0, this)\" oncontextmenu=\"setAbilityFilter(302999, 0, this)\">Arcane Vulnerability</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=302999\">ID: 302999</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-127\" style=\"\" onclick=\"return setAbilityFilter(303797, 0, this)\" oncontextmenu=\"setAbilityFilter(303797, 0, this)\">Beckon</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=303797\">ID: 303797</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-127\" style=\"\" onclick=\"return setAbilityFilter(302141, 0, this)\" oncontextmenu=\"setAbilityFilter(302141, 0, this)\">Beckon</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=302141\">ID: 302141</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-127\" style=\"\" onclick=\"return setAbilityFilter(299094, 0, this)\" oncontextmenu=\"setAbilityFilter(299094, 0, this)\">Beckon</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=299094\">ID: 299094</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(301078, 0, this)\" oncontextmenu=\"setAbilityFilter(301078, 0, this)\">Charged Spear</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=301078\">ID: 301078</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-8\" style=\"\" onclick=\"return setAbilityFilter(298014, 0, this)\" oncontextmenu=\"setAbilityFilter(298014, 0, this)\">Cold Blast</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=298014\">ID: 298014</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(303825, 0, this)\" oncontextmenu=\"setAbilityFilter(303825, 0, this)\">Crushing Depths</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=303825\">ID: 303825</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-32\" style=\"\" onclick=\"return setAbilityFilter(297907, 0, this)\" oncontextmenu=\"setAbilityFilter(297907, 0, this)\">Cursed Heart</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=297907\">ID: 297907</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-32\" style=\"\" onclick=\"return setAbilityFilter(48743, 0, this)\" oncontextmenu=\"setAbilityFilter(48743, 0, this)\">Death Pact</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=48743\">ID: 48743</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(298569, 0, this)\" oncontextmenu=\"setAbilityFilter(298569, 0, this)\">Drained Soul</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=298569\">ID: 298569</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(300866, 0, this)\" oncontextmenu=\"setAbilityFilter(300866, 0, this)\">Essence of Azeroth</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=300866\">ID: 300866</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-16\" style=\"\" onclick=\"return setAbilityFilter(298018, 0, this)\" oncontextmenu=\"setAbilityFilter(298018, 0, this)\">Frozen</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=298018\">ID: 298018</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(299252, 0, this)\" oncontextmenu=\"setAbilityFilter(299252, 0, this)\">March!</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=299252\">ID: 299252</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-32\" style=\"\" onclick=\"return setAbilityFilter(303981, 0, this)\" oncontextmenu=\"setAbilityFilter(303981, 0, this)\">Nether Portal</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=303981\">ID: 303981</a></td></tr></tbody></table><table class=\"actor-menu-table \"><tbody><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-0\" style=\"\" onclick=\"return setAbilityFilter(252753, 0, this)\" oncontextmenu=\"setAbilityFilter(252753, 0, this)\">Potion of Replenishment</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=252753\">ID: 252753</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(225080, 0, this)\" oncontextmenu=\"setAbilityFilter(225080, 0, this)\">Reincarnation</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=225080\">ID: 225080</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(160029, 0, this)\" oncontextmenu=\"setAbilityFilter(160029, 0, this)\">Resurrecting</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=160029\">ID: 160029</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-64\" style=\"\" onclick=\"return setAbilityFilter(299276, 0, this)\" oncontextmenu=\"setAbilityFilter(299276, 0, this)\">Sanction</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=299276\">ID: 299276</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(57724, 0, this)\" oncontextmenu=\"setAbilityFilter(57724, 0, this)\">Sated</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=57724\">ID: 57724</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(298756, 0, this)\" oncontextmenu=\"setAbilityFilter(298756, 0, this)\">Serrated Edge</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=298756\">ID: 298756</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-32\" style=\"\" onclick=\"return setAbilityFilter(187464, 0, this)\" oncontextmenu=\"setAbilityFilter(187464, 0, this)\">Shadow Mend</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=187464\">ID: 187464</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(299255, 0, this)\" oncontextmenu=\"setAbilityFilter(299255, 0, this)\">Stand Alone!</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=299255\">ID: 299255</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(299254, 0, this)\" oncontextmenu=\"setAbilityFilter(299254, 0, this)\">Stand Together!</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=299254\">ID: 299254</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(300492, 0, this)\" oncontextmenu=\"setAbilityFilter(300492, 0, this)\">Static Shock</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=300492\">ID: 300492</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(299253, 0, this)\" oncontextmenu=\"setAbilityFilter(299253, 0, this)\">Stay!</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=299253\">ID: 299253</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(299249, 0, this)\" oncontextmenu=\"setAbilityFilter(299249, 0, this)\">Suffer!</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=299249\">ID: 299249</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-1\" style=\"\" onclick=\"return setAbilityFilter(209261, 0, this)\" oncontextmenu=\"setAbilityFilter(209261, 0, this)\">Uncontained Fel</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=209261\">ID: 209261</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-8\" style=\"\" onclick=\"return setAbilityFilter(303438, 0, this)\" oncontextmenu=\"setAbilityFilter(303438, 0, this)\">Undulating Tides</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=303438\">ID: 303438</a></td></tr><tr><td class=\"ability-menu-name\"><a href=\"#\" class=\"school-2\" style=\"\" onclick=\"return setAbilityFilter(6788, 0, this)\" oncontextmenu=\"setAbilityFilter(6788, 0, this)\">Weakened Soul</a></td><td class=\"ability-menu-id\"><a target=\"_new\" href=\"//www.wowhead.com/spell=6788\">ID: 6788</a></td></tr></tbody></table></ul></li>";
+
+function createXHR() 
+{
+    var request = false;
+        try {
+            request = new ActiveXObject('Msxml2.XMLHTTP');
+        }
+        catch (err2) {
+            try {
+                request = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+            catch (err3) {
+				try {
+					request = new XMLHttpRequest();
+				}
+				catch (err1) 
+				{
+					request = false;
+				}
+            }
+        }
+    return request;
+}
+
+
+function assignImages(fileResponse) 
+{
+	//do stuff
+	console.log(fileResponse);
+
+   	return fileResponse;   
+} 
+
+function loadHTML(url, fun, storage, param, gallery)
+{
+	var xhr = createXHR();
+	xhr.onreadystatechange=function()
+	{ 
+		if(xhr.readyState == 4)
+		{
+			assignImages(xhr.responseText);
+		} 
+	}; 
+	xhr.open("GET", url , true);
+	xhr.send(null); 
+} 
+
+function processHTML(temp, target)
+{
+	target.innerHTML = temp.innerHTML;
+}
+
+function loadWholePage(path, gallery)
+{
+	var y = document.getElementById("storage");
+	var x = document.getElementById("images");
+	loadHTML(path, processHTML, x, y, gallery);
+}
+
+window.onload = function() {
+	loadImageGallery();
+};
+
+function loadImageGallery() {
+	loadWholePage("https://www.warcraftlogs.com/reports/1D6QGwCBg7RPh3TH/");
+}
+
